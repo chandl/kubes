2e1d895c9671797498a1422878fa40b4f6ceceae
Chandler <me@chandl.io>
2021-06-26 13:06:18 -0700

    Update create-timetrack script

--- timetrack/create-timetrack.sh
+++ timetrack/create-timetrack.sh
@@ -14,19 +14,22 @@ sudo k3s kubectl apply -f timetrack-config.yml -n $NAMESPACE
 echo "Installing MariaDB"
 sudo k3s kubectl apply -f mariadb.yml -n $NAMESPACE
 
-echo "Generating MYSQL Root Password"
+echo "Generating MariaDB Root Password"
 TMP_PW=$(sudo mktemp)
 tr -dc 'A-Za-z0-9_#$!@\-' </dev/urandom | head -c 20 | sudo tee -a $TMP_PW
 sudo k3s kubectl create secret generic mysql-root-password -n $NAMESPACE --from-file=password=$TMP_PW
 sudo rm $TMP_PW
 sudo k3s kubectl describe secrets/mysql-root-password -n $NAMESPACE
 
-echo "Generating MYSQL Timetrack Password"
+echo "Generating MariaDB Timetrack Password"
 TMP_PW=$(sudo mktemp)
 tr -dc 'A-Za-z0-9_#$!@\-' </dev/urandom | head -c 20 | sudo tee -a $TMP_PW
 sudo k3s kubectl create secret generic mysql-timetrack-password -n $NAMESPACE --from-file=password=$TMP_PW
 sudo rm $TMP_PW
 sudo k3s kubectl describe secrets/mysql-timetrack-password -n $NAMESPACE
+
+echo "Deploying MariaDB"
+sudo k3s kubectl apply -f mariadb.yml -n $NAMESPACE
 
 echo "Deploying Timetrack Service"
 sudo k3s kubectl apply -f timetrack.yml -n $NAMESPACE
